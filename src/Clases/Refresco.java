9146609961752720179ffa92576ddd7c85b6ab73
usuario <usuario@DESKTOP-VH3U41C>
2022-12-27 17:51:07 +0100

    Atributos y metodos creados

--- src/Clases/Refresco.java
+++ src/Clases/Refresco.java
@@ -1,5 +1,18 @@
 package Clases;
 
 public class Refresco {
-
+	
+	private String sabor;
+	private Boolean zumo;
+	private Boolean gaseoso;
+	private Integer cantidadAzucar;
+	
+	public static void visualizarPropiedades() {
+		
+	}
+	
+	public static Boolean esSaludable() {
+		return null;
+		
+	}
 }
