12d114154a0181944615c2770e5b4973c942f659
usuario <usuario@DESKTOP-VH3U41C>
2022-12-27 17:37:15 +0100

    Atributos y Metodos de la clase

---
+++ src/Clases/Factura.java
@@ -0,0 +1,33 @@
+package Clases;
+
+import java.util.Date;
+
+public class Factura {
+	
+	static Integer IVA = 21;
+	private Integer numero;
+	private String nombreEmpresa;
+	private Date fecha;
+	private String concepto;
+	
+	public static void addLinea(LineaFactura lineaFactura) {
+		
+	}
+	
+	public static void eliminarLinea(Integer numeroLinea) {
+		
+	}
+	
+	public static Double precioTotal() {
+		return null;
+		
+	}
+	
+	public static void mostrarEnPantalla() {
+		
+	}
+	
+	public static void guardarEnFichero() {
+		
+	}
+}
