cd43255ba2c82f1f451a4ac758ba1e235d20c232
usuario <usuario@DESKTOP-VH3U41C>
2022-12-27 17:06:37 +0100

    Metodos

--- src/Clases/Alcoholico.java
+++ src/Clases/Alcoholico.java
@@ -4,5 +4,11 @@ public class Alcoholico {
 	static final double TASA_Bebidas_Fuertes = 64.13;
 	static final double TASA_Bebidas_Suaves = 38.48;
 	
+	public static void esFuerte() {
+		
+	}
 	
+	public static void calcularTasa() {
+		
+	}
 }
