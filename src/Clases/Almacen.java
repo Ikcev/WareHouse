406a9873b94811fe4caa56f0c4f08222c1ab1034
usuario <usuario@DESKTOP-VH3U41C>
2022-12-27 17:19:32 +0100

    Metodos, falta pulir los que devuelven ArrayList

--- src/Clases/Almacen.java
+++ src/Clases/Almacen.java
@@ -1,5 +1,53 @@
 package Clases;
 
-public class Almacen {
+import java.util.ArrayList;
 
+public class Almacen {
+	
+	public static void cargarDatos() {
+		
+	}
+	
+	public static Articulo elMasCaro() {
+		return null;
+		
+	}
+	
+	public static Double precio() {
+		return null;
+		
+	}
+	
+	public static Boolean hayStock() {
+		return null;
+		
+	}
+	
+	public static ArrayList stockJusto() {
+		return null;
+		
+	}
+	
+	public static void articulo(String codigoProducto) {
+		
+	}
+	
+	public static Boolean disponibilidad(Integer cantidad, String kodigoProducto) {
+		return null;
+		
+	}
+	
+	public static ArrayList equivalentes(Articulo articulo) {
+		return null;
+		
+	}
+	
+	public static ArrayList ordenarPorPrecio(String orden) {
+		return null;
+		
+	}
+	
+	public static void ordenarPorStock (String orden) {
+		
+	}
 }
