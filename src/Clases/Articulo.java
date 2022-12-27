de39876baeb4488fafece60f592a1678f5651196
usuario <usuario@DESKTOP-VH3U41C>
2022-12-27 17:29:01 +0100

    Clase Abstracta con sus atributos y metodos

--- src/Clases/Articulo.java
+++ src/Clases/Articulo.java
@@ -1,5 +1,32 @@
 package Clases;
 
-public class Articulo {
-
+public abstract class Articulo {
+	
+	private String code;
+	private String name;
+	private String mark;
+	private Integer capacidadBotella;
+	private Double precio;
+	private Integer stock;
+	
+	public static void incrementarStock() {
+		
+	}
+	
+	public static void disminuirStock(Integer cantidad) {
+		
+	}
+	
+	public static void visualizarArticulo() {
+		
+	}
+	
+	public static Boolean saludable() {
+		return null;
+		
+	}
+	
+	public static void precioTotal() {
+		
+	}
 }
