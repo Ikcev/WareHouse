1c35f8b9067b144203ac91996400f3c5a8ec1f98
usuario <usuario@DESKTOP-VH3U41C>
2022-12-27 17:08:28 +0100

    Cambio en los metodos de void a boolena y double

--- src/Clases/Alcoholico.java
+++ src/Clases/Alcoholico.java
@@ -4,11 +4,13 @@ public class Alcoholico {
 	static final double TASA_Bebidas_Fuertes = 64.13;
 	static final double TASA_Bebidas_Suaves = 38.48;
 	
-	public static void esFuerte() {
+	public static Boolean esFuerte() {
+		return null;
 		
 	}
 	
-	public static void calcularTasa() {
+	public static Double calcularTasa() {
+		return null;
 		
 	}
 }
