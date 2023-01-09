7e047e512d42796aced614d7d53e4f34292112fc
Alejandro Gomez <Alejandro Gomez@HZ114823.H012108.NET>
2023-01-09 13:06:23 +0100

    Getter y setters

--- src/Clases/Vino.java
+++ src/Clases/Vino.java
@@ -8,4 +8,35 @@ public class Vino implements Alcoholico {
 	private String tipoUva;
 	private Double gradosAlcohol;
 	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getOrigen() {
+		return origen;
+	}
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+	public Integer getAnio() {
+		return anio;
+	}
+	public void setAnio(Integer anio) {
+		this.anio = anio;
+	}
+	public String getTipoUva() {
+		return tipoUva;
+	}
+	public void setTipoUva(String tipoUva) {
+		this.tipoUva = tipoUva;
+	}
+	public Double getGradosAlcohol() {
+		return gradosAlcohol;
+	}
+	public void setGradosAlcohol(Double gradosAlcohol) {
+		this.gradosAlcohol = gradosAlcohol;
+	}
+	
 }
