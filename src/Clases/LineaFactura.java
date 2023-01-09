11b9183d890b0f62986789938d6ef7483662fc62
Alejandro Gomez <Alejandro Gomez@HZ114823.H012108.NET>
2023-01-09 13:08:20 +0100

    Getters y Setters

--- src/Clases/LineaFactura.java
+++ src/Clases/LineaFactura.java
@@ -6,4 +6,28 @@ public class LineaFactura {
 	private Integer cantidad;
 	
 	public static Double precioTotal;
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public Articulo getArticulo() {
+		return articulo;
+	}
+
+	public void setArticulo(Articulo articulo) {
+		this.articulo = articulo;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
 }
