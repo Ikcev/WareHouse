074feb24405cf2e5fb4a3741ea1895c545036eb0
Alejandro Gomez <Alejandro Gomez@HZ114823.H012108.NET>
2023-01-09 13:10:10 +0100

    Getters y Setters

--- src/Clases/Articulo.java
+++ src/Clases/Articulo.java
@@ -9,6 +9,54 @@ public abstract class Articulo {
 	private Double precio;
 	private Integer stock;
 	
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMark() {
+		return mark;
+	}
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+
+	public Integer getCapacidadBotella() {
+		return capacidadBotella;
+	}
+
+	public void setCapacidadBotella(Integer capacidadBotella) {
+		this.capacidadBotella = capacidadBotella;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
 	public static void incrementarStock() {
 		
 	}
