7d784e24703555a6799216e4bb29358217218459
Alejandro Gomez <Alejandro Gomez@HZ114823.H012108.NET>
2023-01-09 13:09:39 +0100

    Getters y Setters

--- src/Clases/Cerveza.java
+++ src/Clases/Cerveza.java
@@ -5,4 +5,23 @@ public class Cerveza implements Alcoholico{
 	private String cereales;
 	private Double gradosAlcohol;
 	
+	public String getOrigen() {
+		return origen;
+	}
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+	public String getCereales() {
+		return cereales;
+	}
+	public void setCereales(String cereales) {
+		this.cereales = cereales;
+	}
+	public Double getGradosAlcohol() {
+		return gradosAlcohol;
+	}
+	public void setGradosAlcohol(Double gradosAlcohol) {
+		this.gradosAlcohol = gradosAlcohol;
+	}
+	
 }
